535b8a0812bf84aa6fed1ae1dd5dd4f21ae4021b
Paul Pgt <paul.putegnat@edu.devinci>
2020-10-03 18:04:14 +0200

    added player attack

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour{
+
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
+
+    public Transform attackPos;
+    public LayerMask whatIsEnemies;
+    public Animator camAnim;
+    public float attackRange;
+    public int damage;
+
+    void Update(){
+
+        if (timeBtwAttack <= 0){
+            //Possibilité d'attaquer
+            if(Input.GetKey(KeyCode.A)) {
+                camAnim.SetTrigger("shake");
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+                for (int i = 0; i < enemiesToDamage.Length; i++){
+                    enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
+                }            
+            }
+
+            timeBtwAttack = startTimeBtwAttack;
+        } else {
+            timeBtwAttack -= Time.deltaTime;
+        }
+    }
+
+    void OnDrawGizmosSelected(){
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+
+    }
+
+}
