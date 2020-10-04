0c1afcd29f8eac52a693e0e68221002ff1e98804
Paul Pgt <paul.putegnat@edu.devinci>
2020-10-04 11:57:08 +0200

    Added Dust and Camera Shake

--- Assets/Mob/Enemy.cs
+++ Assets/Mob/Enemy.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
 
+    public GameObject dust;
     public int vie = 100;
     public void TakeDamage(int damage)
     {
         vie -= damage;
+        Instantiate(dust, transform.position, Quaternion.identity);
     }
 
     void Update(){
